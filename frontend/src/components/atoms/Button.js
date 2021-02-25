107ed72b0893cb4c65746a585739060d168cbae8
GeduardoOC <3duardo.oc@gmail.com>
2021-02-25 19:58:13 -0400

    fix buttons

--- frontend/src/components/atoms/Button.js
+++ frontend/src/components/atoms/Button.js
@@ -8,7 +8,7 @@ export default function Button() {
     <div className="container m-0">
       <div className="row">
         <div className="col">
-          <button className="btn btn-primary col-12" type="button">
+          <button className="btn btn-primary col-12 border-0" type="button">
             CONTACT US
           </button>
         </div>
--- frontend/src/pages/App.js
+++ frontend/src/pages/App.js
@@ -11,10 +11,13 @@ export default function App() {
   return (
     <div>
       <ButtonsSendAndLogin
-        styles="btn btn-primary col-12 login"
+        styles="btn btn-primary col-12 login border-0"
         text="Log in"
       />
-      <ButtonsSendAndLogin styles="btn btn-primary col-12 send" text="Send" />
+      <ButtonsSendAndLogin
+        styles="btn btn-primary col-12 send border-0"
+        text="Send"
+      />
     </div>
   );
 }
